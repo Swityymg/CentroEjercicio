dd76860e24d7f08d481a792d21a3f8ad43e1643d
Swietenia Medina <123201686+Swityymg@users.noreply.github.com>
2025-05-14 12:19:49 -0500

    Update Footer.jsx

--- allegra.frontend/src/components/Footer.jsx
+++ allegra.frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
           <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <div className="col-md-4 d-flex align-items-center">
               <span className="mb-3 mb-md-0 text-body-secondary">
-                © 2024 Allegra Studio, Inc
+                © 2024 Allegra Studio, Inc.
               </span>
             </div>
             <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
